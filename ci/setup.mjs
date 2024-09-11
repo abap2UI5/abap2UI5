e14818fec820926eb03d9df2ab8bafbf50dbdb3f
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2024-09-11 21:22:21 +0000

    update

--- ci/setup.mjs
+++
@@ -1,8 +0,0 @@
-import {SQLiteDatabaseClient} from "@abaplint/database-sqlite";
-
-export async function setup(abap, schemas, insert) {
-  abap.context.databaseConnections["DEFAULT"] = new SQLiteDatabaseClient();
-  await abap.context.databaseConnections["DEFAULT"].connect();
-  await abap.context.databaseConnections["DEFAULT"].execute(schemas.sqlite);
-  await abap.context.databaseConnections["DEFAULT"].execute(insert);
-}
