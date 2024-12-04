a7b2269e3283d96abd661eaafef8859395c046d3
Christian Wildt <christian.wildt@googlemail.com>
2024-12-04 20:10:08 +0100

    Update View1.controller.js

--- app/webapp/controller/View1.controller.js
+++ app/webapp/controller/View1.controller.js
@@ -325,19 +325,19 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/mvc/XMLView", "sap/ui/
                         break;
                     case 'URLHELPER':
                         var URLHelper = mobileLibrary.URLHelper;
-                        var obj = JSON.parse(JSON.stringify(args[2]));
+                        var params = args[2];
                         switch (args[1]) {
                             case 'REDIRECT':
-                              URLHelper.redirect(obj.URL, obj.NEW_WINDOW);
+                              URLHelper.redirect(params.URL, params.NEW_WINDOW);
                               break;
                             case 'TRIGGER_EMAIL':
-                              URLHelper.triggerEmail(obj.EMAIL, obj.SUBJECT, obj.BODY, obj.CC, obj.BCC, obj.NEW_WINDOW);
+                              URLHelper.triggerEmail(params.EMAIL, params.SUBJECT, params.BODY, params.CC, params.BCC, params.NEW_WINDOW);
                               break;
                             case 'TRIGGER_SMS':
-                              URLHelper.triggerSms(args[2]);
+                              URLHelper.triggerSms(params);
                               break;
                             case 'TRIGGER_TEL':
-                              URLHelper.triggerTel(args[2]);
+                              URLHelper.triggerTel(params);
                               break;
                         }
                         break;
